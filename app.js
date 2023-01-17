4b7b17850b5223de25525dc124e5699fa9004e29
Mushmuroka <nedyalko.todorov94@gmail.com>
2023-01-17 11:22:57 +0100

    a

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const {SendEmail} = require('./Nodemailer')
+
+require("dotenv").config()
+const app = express()
+app.use(express.json())
+
+
+app.post('/',async(req,res)=>{
+    console.log(req.body)
+
+    const html = ` <h1> Subject: ${req.body.subject}</h1>
+                    <h4>Send from :${req.body.email}</h4>
+                    <p>${req.body.message}</p>`
+   try {
+    await SendEmail(req.body.subject,html,req.body.message)
+    // req.body.email,
+    res.json({msg:'email was send successfully'})
+    
+   } catch (error) {
+    res.json({msg:'fail to send email'})
+    
+   }
+   
+})
+
+const port = process.env.PORT || 10000
+app.listen(port,()=>{
+    console.log(`server is running at  ${port}`)
+})     
+ 
+//SenderEmail,subject,text
